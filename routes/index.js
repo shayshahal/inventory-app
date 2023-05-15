49fa7751722e7da90d7ef49b53d93c28a1dcdb53
shayshahal <shay840@gmail.com>
2023-05-15 13:04:13 +0300

    skeleton setup

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const asyncHandler = require('express-async-handler');
+
+const itemController = require('../controllers/itemController');
+const categoryController = require('../controllers/categoryController');
+const bundleController = require('../controllers/bundleController');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get(
+	'/',
+	asyncHandler(async (req, res, next) => {
+		res.render('index', { title: 'Home' });
+	})
+);
+
 
 module.exports = router;
