bf1a4f3fff338f4e014fa92e1514148fd292ac35
shayshahal <shay840@gmail.com>
2023-05-29 12:26:38 +0300

    add item creation post route

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -40,3 +40,65 @@ exports.item_create_get = asyncHandler(async (req, res, next) => {
 		categories: allCategories,
 	});
 });
+
+exports.item_create_post = [
+	// Convert the category to an array.
+	(req, res, next) => {
+		if (!(req.body.category instanceof Array)) {
+			if (typeof req.body.category === 'undefined')
+				req.body.category = [];
+			else req.body.category = new Array(req.body.category);
+		}
+		next();
+	},
+	// Validate and sanitize fields.
+	body('name', 'Name must be at least 2 characters.')
+		.trim()
+		.isLength({ min: 2 })
+		.escape(),
+	body('price', 'Must enter a price.')
+		.trim()
+		.notEmpty()
+		.if(body('price').notEmpty())
+		.isInt({ gt: 0 })
+		.withMessage('Price must be larger than zero.')
+		.escape(),
+	asyncHandler(async (req, res, next) => {
+		const errors = validationResult(req);
+
+		const itemDetail = {
+			name: req.body.name,
+			price: req.body.price,
+			stats: {
+				'Magic Power': req.body.magic_power,
+				'Attack Damage': req.body.attack_damage,
+				'Attack Speed': req.body.attack_speed,
+				'Critical Strike Chance': req.body.crit_chance,
+			},
+			categories: req.body.category,
+		};
+		if (req.body.image) itemDetail.image = req.body.image;
+		const item = new Item(itemDetail);
+		console.log(item);
+
+		if (!errors.isEmpty()) {
+			const allCategories = await Category.find().exec();
+
+			for (const category of allCategories) {
+				if (item.categories.indexOf(category._id) > -1) {
+					category.checked = 'true';
+				}
+			}
+
+			res.render('item_form', {
+				title: 'Create a new item',
+				categories: allCategories,
+				item: item,
+				errors: errors.array(),
+			});
+		} else {
+			await item.save();
+			res.redirect(item.url);
+		}
+	}),
+];
