a46cb09fd08230e95f4e717253a4e99ed41a6ea7
shayshahal <shay840@gmail.com>
2023-05-30 17:00:31 +0300

    add uploads route

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use('/uploads', express.static('uploads'));
 app.use(
 	helmet.contentSecurityPolicy({
 		directives: {
