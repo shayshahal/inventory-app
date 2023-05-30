2be47fe36ea9ed8b4a7258f8efd99ad7b2551f66
shayshahal <shay840@gmail.com>
2023-05-30 10:24:03 +0300

    uncomment all routes

--- routes/index.js
+++ routes/index.js
@@ -22,13 +22,13 @@ router.get('/item/create', itemController.item_create_get);
 
 router.post('/item/create', itemController.item_create_post);
 
-// router.get('/item/:id/update', itemController.item_update_get);
+router.get('/item/:id/update', itemController.item_update_get);
 
-// router.post('/item/:id/update', itemController.item_update_post);
+router.post('/item/:id/update', itemController.item_update_post);
 
-// router.get('/item/:id/delete', itemController.item_delete_get);
+router.get('/item/:id/delete', itemController.item_delete_get);
 
-// router.post('/item/:id/delete', itemController.item_delete_post);
+router.post('/item/:id/delete', itemController.item_delete_post);
 
 router.get('/item/:id', itemController.item_detail);
 
@@ -36,17 +36,17 @@ router.get('/item/:id', itemController.item_detail);
 
 router.get('/categories', categoryController.category_list);
 
-// router.get('/category/create', categoryController.category_create_get);
+router.get('/category/create', categoryController.category_create_get);
 
-// router.post('/category/create', categoryController.category_create_post);
+router.post('/category/create', categoryController.category_create_post);
 
-// router.get('/category/:id/update', categoryController.category_update_get);
+router.get('/category/:id/update', categoryController.category_update_get);
 
-// router.post('/category/:id/update', categoryController.category_update_post);
+router.post('/category/:id/update', categoryController.category_update_post);
 
-// router.get('/category/:id/delete', categoryController.category_delete_get);
+router.get('/category/:id/delete', categoryController.category_delete_get);
 
-// router.post('/category/:id/delete', categoryController.category_delete_post);
+router.post('/category/:id/delete', categoryController.category_delete_post);
 
 router.get('/category/:id', categoryController.category_detail);
 
@@ -54,17 +54,17 @@ router.get('/category/:id', categoryController.category_detail);
 
 router.get('/bundles', bundleController.bundle_list);
 
-// router.get('/bundle/create', bundleController.bundle_create_get);
+router.get('/bundle/create', bundleController.bundle_create_get);
 
-// router.post('/bundle/create', bundleController.bundle_create_post);
+router.post('/bundle/create', bundleController.bundle_create_post);
 
-// router.get('/bundle/:id/update', bundleController.bundle_update_get);
+router.get('/bundle/:id/update', bundleController.bundle_update_get);
 
-// router.post('/bundle/:id/update', bundleController.bundle_update_post);
+router.post('/bundle/:id/update', bundleController.bundle_update_post);
 
-// router.get('/bundle/:id/delete', bundleController.bundle_delete_get);
+router.get('/bundle/:id/delete', bundleController.bundle_delete_get);
 
-// router.post('/bundle/:id/delete', bundleController.bundle_delete_post);
+router.post('/bundle/:id/delete', bundleController.bundle_delete_post);
 
 router.get('/bundle/:id', bundleController.bundle_detail);
 
